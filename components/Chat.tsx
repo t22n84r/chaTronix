34fb0e8968db3b5806d9fd622f8bc324c0c1e90f
t22n84r <81875062+t22n84r@users.noreply.github.com>
2023-10-28 20:53:11 -0400

    Full-Stack Immersion Achievement-5 Exercise-5.1
    https://careerfoundry.com/en/course/full-stack-immersion/exercise/chat-ui-accessibility#task
    
    Added packages react-native-gifted-chat & react-native-image in package.json
    
    Setup chat.tsx with gifted-chat for chat styling, also implemented other stylings for avatar and username

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -1,5 +1,7 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect } from "react";
+import { StyleSheet, Text, View, Platform, KeyboardAvoidingView, Image } from 'react-native';
 import { RouteProp } from '@react-navigation/native';
+import { GiftedChat, IMessage, Bubble, BubbleProps } from "react-native-gifted-chat";
 import { RootStackParamList } from './Start';
 
 type ChatScreenRouteProp = RouteProp<RootStackParamList, 'Chat'>;
@@ -12,9 +14,93 @@ const Chat: React.FC<Props> = ({ route }) => {
    const {bgColor} = route.params;
    const {userName} = route.params
 
+   const [messages, setMessages] = useState<IMessage[]>([]);
+
+   useEffect(() => {
+      setMessages([
+         {
+            _id: 1,
+            text: 'Hello Developer!',
+            createdAt: new Date(),
+            user: {
+               _id: 2,
+               name: "System",
+               avatar: "https://dummyimage.com/200x200/a2d2ff/0d1321.png&text=System",
+            },
+            system: true,
+         },
+         {
+            _id: 3,
+            text: "Hello developer",
+            createdAt: new Date(),
+            user: {
+            _id: 4,
+            name: "React Native",
+            avatar: "https://reactnative.dev/img/tiny_logo.png",
+            },
+         },
+      ]);
+   }, []);
+
+   const onSend = (newMessages: IMessage[]) => {
+      setMessages(previousMessages => GiftedChat.append(previousMessages, newMessages))
+   }
+   
+   const renderBubble = (props: BubbleProps<IMessage>) => {
+      const username = props.currentMessage?.user?.name;
+  
+      return (
+          <View>
+              {username && <Text style={{ fontWeight: 'bold', marginBottom: 5, color: 'white' }}>{username}</Text>}
+              <Bubble
+                  {...props}
+                  wrapperStyle={{
+                      right: {
+                          backgroundColor: "#000",
+                      },
+                      left: {
+                          backgroundColor: "#FFF",
+                      },
+                  }}
+              />
+          </View>
+      );
+   };
+  
+
+   const renderAvatar = (props: any) => {
+      
+      const user = props.currentMessage ? props.currentMessage.user : props.user;
+      
+      if (user && user.avatar) {
+          return (
+              <Image
+                  source={{ uri: user.avatar }}
+                  style={{ width: 64, height: 64, borderRadius: 50 }}
+              />
+          );
+      }
+      // Handle cases where there's no avatar, perhaps return a default avatar or null
+      return null;
+   };
+
    return (
       <View style={{...styles.container, backgroundColor: bgColor}}>
          <Text style={styles.textStyle}>Hello {userName}!</Text>
+
+         <GiftedChat
+            messages={messages}
+            renderBubble={renderBubble}
+            showUserAvatar={true}
+            showAvatarForEveryMessage={true}
+            renderAvatar={renderAvatar}
+            onSend={(messages: IMessage[]) => onSend(messages)}
+            user={{
+            _id: 1
+            }}
+         />
+
+         { Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null }
       </View>
    );
 };
@@ -22,12 +108,12 @@ const Chat: React.FC<Props> = ({ route }) => {
 const styles = StyleSheet.create({
    container: {
      flex: 1,
-     alignItems: 'center',
-     justifyContent: 'center',
    },
    textStyle: {
       fontSize: 32,
-      color: 'white'
+      color: 'white',
+      textAlign: 'center',
+      marginTop: 10,
    }
  });
 
